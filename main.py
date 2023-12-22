b576a5a4d5575c0fc08c417d3f0a544c27842d15
hattiza <quiet.iron8712@fastmail.com>
2023-12-22 11:46:05 -0800

    Add blank README and CLI prompt

---
+++ main.py
@@ -0,0 +1,11 @@
+# Prompt the user for tasks, day of the week, start time, and end time
+tasks = input("Enter the tasks you do during the day: ")
+day_of_week = input("Enter the day of the week: ")
+start_time = input("Enter the start time: ")
+end_time = input("Enter the end time: ")
+
+# Print the entered information
+print("Tasks:", tasks)
+print("Day of the week:", day_of_week)
+print("Start time:", start_time)
+print("End time:", end_time)
